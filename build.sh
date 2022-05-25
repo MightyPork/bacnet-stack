d1d70aa7efa0a64f4744996a8b9d8523da3f2046
Ondřej Hruška <ondra@ondrovo.com>
2022-05-25 12:46:27 +0200

    build.sh to use all CPU cores and remove wrong windows aliases

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-alias make=mingw32-make.exe
-PATH=/c/MinGW/bin:$PATH
-export MAKE=mingw32-make.exe
-export CC=mingw32-gcc.exe
-export OBJCOPY=objcopy.exe
-export AR=ar.exe
-export SIZE=size.exe
-
-make clean
-make all
+rm -rf _build
+mkdir _build
+cd _build
+cmake ..
+make -j`nproc`
